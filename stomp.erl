b4b130a5adc90bf4c9725e919bdefcebe0e6cc25
igb <igb@phosphorus.hccp.org>
2009-06-30 22:49:47 -0700

    first working copy

--- stomp.erl
+++ stomp.erl
@@ -1,8 +1,33 @@
 -module (stomp).
--export ([connect/3]).
+-export ([connect/4]).
 
 
-connect (HOST, LOGIN, PASSCODE)  ->
-	io:format("HOST:~w~nLOGIN:~w~nPASSCODE:~w~n", [HOST, LOGIN, PASSCODE]),
-	MESSAGE="CONNECT",
-	io:format("~w", [MESSAGE]).
+%% stomp:connect("localhost", 61613, "", "").
+
+connect (Host, PortNo, Login, Passcode)  ->
+	io:format("HOST:~w~nLOGIN:~w~nPASSCODE:~w~n", [Host, Login, Passcode]),
+	Connect="CONNECT",
+	Message=lists:append([Connect, "\nlogin: ", Login, "\npasscode: ", Passcode, "\n\n", [0]]),
+	{ok,Sock}=gen_tcp:connect(Host,PortNo,[{active, false}]),
+	io:format("Socket=~p~n",[Sock]),
+	gen_tcp:send(Sock,Message),
+	A=gen_tcp:recv(Sock, 0),
+	io:format("~w",[A]),
+	gen_tcp:close(Sock).
+
+
+	do_recv(Sock, Bs) ->
+	    case gen_tcp:recv(Sock, 0) of
+	        {ok, B} ->
+	            do_recv(Sock, [Bs, B]);
+	        {error, closed} ->
+	            {ok, list_to_binary(Bs)}
+	    end.
+
+wait_reply(X) ->
+	receive
+		Reply ->
+			{value, Reply}
+			after 1000 ->
+				timeout
+			end.
