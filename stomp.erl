5e2fecc6c975440a6578a030a1f4615dd9e1551f
igb <igb@phosphorus.mygazoo.com>
2009-07-05 13:58:44 -0700

    adding retrival of type from message

--- stomp.erl
+++ stomp.erl
@@ -4,12 +4,12 @@
 -export ([subscribe/2]).
 -export ([subscribe/3]).
 -export ([unsubscribe/2]).
--export ([get_message/1]).
+-export ([get_messages/1]).
 
 
 
 
-%% stomp:connect("localhost", 61613, "", "").
+%% Example:	Conn = stomp:connect("localhost", 61613, "", "").
 
 connect (Host, PortNo, Login, Passcode)  ->
 	Message=lists:append(["CONNECT", "\nlogin: ", Login, "\npasscode: ", Passcode, "\n\n", [0]]),
@@ -18,21 +18,29 @@ connect (Host, PortNo, Login, Passcode)  ->
 	{ok, Response}=gen_tcp:recv(Sock, 0),
 	Sock.
 
+
+%% Example: stomp:subscribe("/queue/foobar", Conn).
+
 subscribe (Destination, Connection) ->
-	subscribe (Destination, Connection, "auto"),
+	subscribe (Destination, Connection, [{"ack","auto"}]),
 	ok.
 
-subscribe (Destination, Connection, Ack) ->
-	Message=lists:append(["SUBSCRIBE", "\ndestination: ", Destination, "\nack: ", Ack,"\n\n", [0]]),
+%%  Example: stomp:subscribe("/queue/foobar", Conn, [{"ack", "client"}]).
+
+subscribe (Destination, Connection, Options) ->
+	Message=lists:append(["SUBSCRIBE", "\ndestination: ", Destination, concatenate_options(Options),"\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.
 	
+
+%% Example: stomp:unsubscribe("/queue/foobar", Conn).
 	
 unsubscribe (Destination, Connection) ->
 	Message=lists:append(["UNSUBSCRIBE", "\ndestination: ", Destination, "\n\n", [0]]),
 	gen_tcp:send(Connection,Message),
 	ok.	
 	
+%% Example: stomp:disconnect(Conn).
 	
 disconnect (Connection) ->
 	Message=lists:append(["DISCONNECT", "\n\n", [0]]),
@@ -40,7 +48,41 @@ disconnect (Connection) ->
 	gen_tcp:close(Connection),
 	ok.	
 
-get_message (Connection) ->
+%% Example: stomp:get_messages(Conn).
+
+get_messages (Connection) ->
 	{ok, Response}=gen_tcp:recv(Connection, 0),
 	io:format("~s", [Response]),
-	Response.
+	Type=get_type(Response),
+%%	Headers=get_headers(nthtail(length(Type) + 1, Response)),
+	io:fwrite("~s", [Type]),
+		[].
+
+%% PRIVATE METHODS . . .	
+concatenate_options ([]) ->
+	[];
+concatenate_options ([H|T]) ->
+	{Name, Value}=H,
+	lists:append(["\n", Name, ": ", Value, concatenate_options(T)]).
+
+
+% MESSAGE PARSING  . . . get's a little ugly in here . . . would help if I truly grokked Erlang, I suspect.
+
+%% extract type ("MESSAGE", "CONNECT", etc.) from message string . . .	
+
+get_type(Message) ->
+	get_type (Message, []).
+	
+get_type ([], Type) ->
+		Type;
+get_type ([H|T], Type) ->	
+		case (H) of
+			10 -> Type;
+			_ -> get_type(T, lists:append([Type, [H]]))	
+		end.
+		
+%% extract headers . . . 
+%%get_headers ([H|T], Headers, LastChar) ->
+%%	case ({H, LastChar}) of
+%%			{10, 10} -> Headers,
+	
