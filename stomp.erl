e171942875634a5bf5446a8ebe12985ef590d3f2
igb <igb@phosphorus.mygazoo.com>
2009-07-17 22:14:21 -0700

    removing debug . . . cleaning up compile warnings

--- stomp.erl
+++ stomp.erl
@@ -10,18 +10,13 @@
 -export ([send/4]).
 
 
-
-
-
-
-
 %% Example:	Conn = stomp:connect("localhost", 61613, "", "").
 
 connect (Host, PortNo, Login, Passcode)  ->
 	Message=lists:append(["CONNECT", "\nlogin: ", Login, "\npasscode: ", Passcode, "\n\n", [0]]),
 	{ok,Sock}=gen_tcp:connect(Host,PortNo,[{active, false}]),
 	gen_tcp:send(Sock,Message),
-	{ok, Response}=gen_tcp:recv(Sock, 0),
+	{ok, _}=gen_tcp:recv(Sock, 0), %%TODO: handle errors/throw exception on error . . . 
 	Sock.
 
 
@@ -103,22 +98,14 @@ get_messages (Connection) ->
 	
 get_messages (Connection, Messages) ->
 	{ok, Response}=gen_tcp:recv(Connection, 0),
-%%	io:format("~s", [Response]),
-%%	io:format("The beginning ~B", [length(Response)]),
-%%	[{type, Type}, {headers, Headers}, {body, MessageBody}, TheRest]=get_message(Response),
 	get_messages(Connection, Messages, Response).
 		
-get_messages (Connection, Messages, []) ->
+get_messages (_, Messages, []) ->
 	Messages;
 get_messages (Connection, Messages, Response) ->
 			[{type, Type}, {headers, Headers}, {body, MessageBody}, TheRest]=get_message(Response),
-			[H|T]=TheRest, %% UGLY
+			[_|T]=TheRest, %% U.G.L.Y. . . .  you ain't got no alibi.
 			get_messages (Connection, lists:append(Messages, [[{type, Type}, {headers, Headers}, {body, MessageBody}]]), T).
-	%%get_messages(Response, [])	
-	
-	%%io:fwrite("Type: ~s", [Type]),
-	%%io:fwrite("~nHeaders:~n~s", [Headers]),
-	%%io:fwrite("~nMessageBody:~n~s", [MessageBody]),
 
 
 %% PRIVATE METHODS . . .	
@@ -132,91 +119,51 @@ concatenate_options ([H|T]) ->
 % MESSAGE PARSING  . . . get's a little ugly in here . . . would help if I truly grokked Erlang, I suspect.
 % 7/12/09 - yeah, ugly indeed, i need to make this use the same pattern as get_headers_from_raw_src . . . currently scanning header block multiple times and making unnecessary copies
 get_message(Message) ->
- 	[Type, {Headers, MessageBody}, TheRest]=get_type2(Message), %% Ugly . . .
+ 	[Type, {Headers, MessageBody}, TheRest]=get_type(Message), %% Ugly . . .
 	{ParsedHeaders, _}=get_headers_from_raw_src([], Headers),
-		%%io:fwrite("returning", []),
 	[{type, Type}, {headers, ParsedHeaders}, {body, MessageBody}, TheRest].
 
 
 
 
-
-%% extract type ("MESSAGE", "CONNECT", etc.) from message string . . .	
-
-get_type(Message) ->
-	get_type (Message, []).
 	
-get_type ([], Type) ->
-		Type;
-get_type ([H|T], Type) ->	
-		case (H) of
-			10 -> [Type, get_headers(T)];
-			_ -> get_type(T, lists:append([Type, [H]]))	
-		end.
-		
+%% extract message body
+get_message_body ([H|T]) ->
+		get_message_body ([H|T], []).
+
+get_message_body ([H|T], MessageBody) ->
+	case(H) of
+		0 -> {MessageBody, T};
+		_ -> {MyMessageBody, TheRest}=get_message_body(T, MessageBody), {lists:append([MessageBody, [H], MyMessageBody]), TheRest}
+	end.	
+
+
 %% extract headers as a blob of chars, after having iterated over . . .
 
 get_headers (Message) ->
 	get_headers (Message, []).
-	
+
 get_headers (Message, Headers) ->
 	get_headers (Message, Headers, -1).
-				
 get_headers ([H|T], Headers, LastChar) ->
 	case ({H, LastChar}) of
-		{10, 10} -> {Headers, get_message_body(T)};
+		{10, 10} -> {MessageBody, TheRest}=get_message_body(T),[{Headers, MessageBody}, TheRest];
 		{_, _} -> get_headers(T, lists:append([Headers, [H]]), H)
-	end.
-	
-%% extract message body
-get_message_body ([H|T]) ->
-	get_message_body ([H|T], []).
-	
-get_message_body ([H|T], MessageBody) ->
-	case(H) of
-		0 -> MessageBody;
-		_ -> lists:append([MessageBody, [H], get_message_body(T, MessageBody)])
-	end.	
-	
-	
-%% extract message body
-get_message_body2 ([H|T]) ->
-		get_message_body2 ([H|T], []).
-
-get_message_body2 ([H|T], MessageBody) ->
-	case(H) of
-		0 -> {MessageBody, T};
-		_ -> {MyMessageBody, TheRest}=get_message_body2(T, MessageBody), {lists:append([MessageBody, [H], MyMessageBody]), TheRest}
-	end.	
-
-
-%% extract headers as a blob of chars, after having iterated over . . .
-
-get_headers2 (Message) ->
-	get_headers2 (Message, []).
-
-get_headers2 (Message, Headers) ->
-%%	io:fwrite("get_headers2...", []),
-	get_headers2 (Message, Headers, -1).
-get_headers2 ([H|T], Headers, LastChar) ->
-	case ({H, LastChar}) of
-		{10, 10} -> {MessageBody, TheRest}=get_message_body2(T),[{Headers, MessageBody}, TheRest];
-		{_, _} -> get_headers2(T, lists:append([Headers, [H]]), H)
 	end.
 	
 	
 	
 %% extract type ("MESSAGE", "CONNECT", etc.) from message string . . .	
 
-get_type2(Message) ->
-	get_type2 (Message, []).
+get_type(Message) ->
+	get_type (Message, []).
 
-get_type2 ([], Type) ->
+get_type ([], Type) ->
 	Type;
-get_type2 ([H|T], Type) ->	
+get_type ([H|T], Type) ->	
 	case (H) of
-		10 -> [{Headers, MessageBody}, TheRest]=get_headers2(T), [Type, {Headers, MessageBody}, TheRest];
-		_ -> get_type2(T, lists:append([Type, [H]]))	
+		10 -> [{Headers, MessageBody}, TheRest]=get_headers(T), [Type, {Headers, MessageBody}, TheRest];
+		_ -> get_type(T, lists:append([Type, [H]]))	
 	end.	
 
 
@@ -230,20 +177,16 @@ get_headers_from_raw_src(Headers, RawSrc) ->
 get_header (RawSrc) ->
 	{HeaderName, RestOfListAfterHeaderExtraction}=get_header_name([], RawSrc),
 	{HeaderValue, RestOfListAfterValueExtraction}=get_header_value([], RestOfListAfterHeaderExtraction),
-%%	io:fwrite("Header: ~s,~s~n", [HeaderName, HeaderValue]),
 	{{HeaderName, HeaderValue}, RestOfListAfterValueExtraction}.
 	
 	
 get_header_name (HeaderName, [H|T]) ->
-	%% io:fwrite("In get_header_name: ~c~n", [H]),
 		case (H) of 
 			58 ->  {HeaderName, T};
 			_ -> get_header_name(lists:append([HeaderName, [H]]), T)
 		end.
 	
 get_header_value (HeaderValue, [H|T]) ->
-	%%	io:fwrite("In get_header_value: ~c~n", [H]),
-		
 		case (H) of 
 			10 -> {HeaderValue, T};
 			_ -> get_header_value(lists:append([HeaderValue, [H]]), T)
